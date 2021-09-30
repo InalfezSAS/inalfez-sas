d1c2d20fcba90bd0186b8da28fa3d55e7e89aab1
Michael Rodriguez <maiikol1192@gmail.com>
2021-09-30 00:43:38 -0500

    Cambios

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ var app = new Vue({
         
     }
   })
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    slidesPerColumn: 3,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
