1916a00525ee5124cdce1c15dc9386ad5a465dcf
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-10-09 11:40:40 -0500

    Creacion de la card de la seccion nuestro equipo

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
-    }
-  })
 
-  const swiper = new Swiper('.swiper', {
+    }
+})
+
+/* const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
-    slidesPerColumn: 3,
-  
+    slidesPerView: 1,
+    slidesPerColumn: 1,
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     // scrollbar: {
     //   el: '.swiper-scrollbar',
     // },
-  });
+}); */
