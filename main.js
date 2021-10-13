64be3981d7ba7fa26c700175005dda228ac7e2c8
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-10-13 01:17:57 -0500

    cambios

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ var personal = new Swiper(".personal", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+var quienesSomos = new Swiper(".quienesSomos", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
